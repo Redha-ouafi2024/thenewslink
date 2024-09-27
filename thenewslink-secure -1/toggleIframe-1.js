fdc537fccef261fa202ca6a89f2cbc257b21ce5c
Redha-ouafi2024 <tonyredharussian@gmail.com>
2024-09-27 15:25:54 +0000

    Add files via upload

---
+++ thenewslink-secure -1/toggleIframe-1.js
@@ -0,0 +1,8 @@
+     function toggleIframe() {
+            const iframe = document.getElementById("myiframe");
+            if (iframe.style.display === "none") {
+                iframe.style.display = "block";
+            } else {
+                iframe.style.display = "none";
+            }
+        }
---
+++ thenewslink-secure -1/toggleIframe-4.js
@@ -0,0 +1,8 @@
+        function toggleIframee() {
+            const iframe = document.getElementById("text-containeree");
+            if (iframe.style.display === "none") {
+                iframe.style.display = "block";
+            } else {
+                iframe.style.display = "none";
+            }
+        }
